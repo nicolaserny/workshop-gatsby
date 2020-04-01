3bfa7cd7df3af0abba88f1919d842b08f46d7de0
nicolaserny <nicolas.erny@xcomponent.com>
2020-04-01 16:39:12 +0200

    Add post images

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,23 @@ module.exports = {
   plugins: [
     'gatsby-plugin-emotion',
     'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-mdx',
       options: {
         defaultLayouts: {
           default: require.resolve('./src/components/layout.js'),
         },
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              quality: 80,
+              wrapperStyle: 'margin-left: 0;',
+            },
+          },
+        ],
       },
     },
     {
@@ -19,6 +30,13 @@ module.exports = {
       options: {
         name: 'posts',
         path: 'posts',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: 'images',
       },
     },
   ],
